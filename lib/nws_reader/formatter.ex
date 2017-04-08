e29f4309aa340ec63b25af541cc43d31a81f1379
Rex Miller <rex.miller@carvana.com>
2017-04-08 11:00:24 -0700

    wip

--- lib/nws_reader/formatter.ex
+++ lib/nws_reader/formatter.ex
@@ -1,5 +1,35 @@
 defmodule NwsReader.Formatter do
-  
+
+  def print_map
+
+  def print_map(map, delimiter \\"|") do
+    IO.inspect(map, label: "map")
+
+    map
+    |> Enum.map(&key_value_list/1)
+    |> Enum.map(&format_key_value_list/1)
+  end
+
+  defp key_value_list({key, value}) when is_map(value), do: [key, print_map(value)]
+  defp key_value_list({key, value}), do: [key, value]
+
+  defp format_key_value_list([key, value]) when is_list(value) do
+     "#{key} , #{print_map(value)}"
+  end
+
+  defp format_key_value_list([key, value]) do
+     "#{key} | #{value}"
+  end
+
+  defp format_pair({key, value}, delimiter) when is_map(value) do
+    "#{key}#{delimiter}#{print_map(value, ": ")}"    
+  end
+
+  defp format_pair({key, value}, delimiter) do
+    "#{key}#{delimiter}#{value}"
+  end
+
+
   def get_columns(map) do
     [ 
       Map.keys(map), 
@@ -13,16 +43,16 @@ defmodule NwsReader.Formatter do
     col_data
     |> List.zip()
     |> IO.inspect(label: "col_data zipped for delimiter(#{delimiter})")
-    |> Enum.map(fn(row) -> render_row(row, delimiter) end)
+    |> Enum.map(fn(row) -> render_pair(row, delimiter) end)
   end
 
-  def render_row(row, delimiter) when is_list(row) do
-    IO.inspect(row, label: "render_row() |> Enum.join(#{delimiter})")
+  def render_pair(row, delimiter) when is_list(row) do
+    IO.inspect(row, label: "render_pair() |> Enum.join(#{delimiter})")
     Enum.join(row, delimiter)
   end
 
-  def render_row(row_tuple, delimiter) do
-    row_tuple
+  def render_pair(key_value_tuple, delimiter) do
+    key_value_tuple
     |> Tuple.to_list()
     |> Enum.map(&render_column/1)
     |> Enum.join(delimiter)
@@ -35,7 +65,7 @@ defmodule NwsReader.Formatter do
     IO.inspect(value, label: "columns_to_rows")
     value
     |> columns_to_rows(": ")
-    |> render_row(", ")
+    |> render_pair(", ")
   end
 
   defp render_column(value, pad_char \\" ", width \\0) do
